4192ae85831b3869d0631a591a7d34395c320ffa
anverumas <155238715+anverumas@users.noreply.github.com>
2024-10-04 15:03:43 +0300

    something

---
+++ script.js
@@ -0,0 +1,18 @@
+async function getDapi() {
+    const url = "https://digi-api.com/api/v1/digimon";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  
+    const element = document.querySelector(".dapi");
+  
+    for (const item of data.content) {
+      const id = item.id;
+      const name = item.name;
+      const image = item.image;
+      element.innerHTML += "<p>" + id + ": " + name + "</p>"
+      element.innerHTML += '<img src="' + image + '">';
+;
+    }
+  }
+getDapi();
